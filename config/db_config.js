7c5003ef1c7b971651ee4a024a0e7e5e49f4d498
msssrp <siripoomcontact@gmail.com>
2023-09-11 09:35:34 +0700

    change env and config

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-  HOST: "aws.connect.psdb.cloud",
-  USER: "iowahtrql4urgeab1r79",
-  PASSWORD: "pscale_pw_s0qIteeVGpQQm3UzSENS57sGKECpUdZWCZFb92WmPSq",
-  DB: "se_database",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
 };
