33f9e893795b8548519961ddb00ba91c6152ca1b
msssrp <siripoomcontact@gmail.com>
2023-09-27 11:13:49 +0700

    changed file name

--- config/db_config.js
+++ config/db_config.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
+  HOST: process.env.CC_HOST,
+  USER: process.env.CC_USER,
+  PASSWORD: process.env.CC_PASSWORD,
+  DB: process.env.CC_DB,
 };
